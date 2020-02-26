3b1fcee518a74069be905c29cc07d475cb010053
GBTC440002UR <GBTC440002UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-26 14:51:43 -0500

    SpringBoot_307 corrected

--- src/main/java/com/cristian/demo/HomeController.java
+++ src/main/java/com/cristian/demo/HomeController.java
@@ -19,10 +19,39 @@ public class HomeController {
         employee.setSsn("555-12-1234");
         Laptop laptop = new Laptop();
 
+        Employee employee1 = new Employee();
+        employee1.setName("");
+        employee1.setSsn("");
+        Laptop laptop1 = new Laptop();
+
+        Employee employee2 = new Employee();
+        employee2.setName("");
+        employee2.setSsn("");
+        Laptop laptop2 = new Laptop();
+
+        Employee employee3 = new Employee();
+        employee3.setName("");
+        employee3.setSsn("");
+        Laptop laptop3 = new Laptop();
+
 
         laptop.setBrand("Dell");
         laptop.setModel("Latitude XL");
+
+        laptop.setBrand("");
+        laptop.setModel("");
+
+        laptop.setBrand("");
+        laptop.setModel("");
+
+        laptop.setBrand("");
+        laptop.setModel("");
+
+
         employeeRepository.save(employee);
+        employeeRepository.save(employee1);
+        employeeRepository.save(employee2);
+        employeeRepository.save(employee3);
 
         model.addAttribute("employees", employeeRepository.findAll());
         return "index";
